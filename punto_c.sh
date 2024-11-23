d6b5038fea7d2e4cb3a49eefe077253e8877befa
Thiaguito2024 <thiagojlgd@gmail.com>
2024-11-23 17:48:27 +0000

    Punto C

--- punto_c.sh
+++ punto_c.sh
@@ -16,3 +16,7 @@ vim contacto.html:
 <h2> Contacto: 112233445566 </h2>
 docker build -t thiago471/homebanking:v1.0 .
 docker push thiago471/homebanking:v1.0
+docker run -d -p 8080:80 thiago471/2parcial-ayso:v1.0
+docker image ls
+docker container ls
+docker stop 9dba5a2243b0
