4a9453fbf3b9d48d1889c5357f5b16f40ba2a753
Thiaguito2024 <thiagojlgd@gmail.com>
2024-11-23 03:56:07 +0000

    Punto C

---
+++ punto_c.sh
@@ -0,0 +1,18 @@
+PARA LA IMAGEN
+docker build -t thiago471/2parcial-ayso:v1.0 .
+docker image ls
+docker push thiago471/2parcial-ayso:v1.0
+
+PARA EL HOME BANKING
+vim dockerfile:
+FROM nginx
+COPY appHomeBanking /usr/share/nginx/html
+vim index.html:
+<h1> Este es el titulo de el index de el parcial </h1>
+vim contacto.html:
+<h1> Numero de contacto </h1>
+<h2> Nombre: Juan </h2>
+<h2> Apellido: Perez </h2>
+<h2> Contacto: 112233445566 </h2>
+docker build -t thiago471/homebanking:v1.0 .
+docker push thiago471/homebanking:v1.0
